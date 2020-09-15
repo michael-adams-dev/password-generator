a87aa7be6889d704b329a02a23f1d576dd632e3b
Michael Adams <michaeladams914.dev@gmail.com>
2020-09-14 19:09:25 -0700

    Getting user input started

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -5,16 +5,25 @@
 # 1. How do we generate a new password in Ruby?
 
 
-
-
-password = ''
-
-10.times do
-  ascii_decimal = rand(33.126)
-  password << ascii_decimal.chr
+def ask_user_for_length
+  puts "How many characters would you like your password to be?"
+  print "> "
+  gets.chomp.to_i
 end
 
-puts password
+
+def generate_password
+  password_length = ask_user_for_length
+  password = ''
+  password_length.times do
+    ascii_decimal = rand(33.126)
+    password += ascii_decimal.chr
+  end
+  return password
+end
+
+puts generate_password
+
 
 
 # 2. How will the user ask for a new password?
